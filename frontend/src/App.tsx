7d81c3901abe76ed684413a2659b7e88ee4865f9
Paulo Stryder <paulim@gmail.com>
2022-01-19 14:28:51 -0300

    Form layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,15 +8,17 @@ import Form from 'pages/Form';
 import Navbar from "components/Navbar";
 
 function App() {
-  return (<BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Listing />} />
-      <Route path="/form">
-        <Route path=":movieId" element={<Form />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 export default App;
